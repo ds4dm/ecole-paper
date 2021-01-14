2f69d35fb1303fea29e985105380c22c9c9049a9
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-14 16:19:20 -0500

    Update Learn2branch observation functions

--- src/ecole_vs_gasse/observation.py
+++ src/ecole_vs_gasse/observation.py
@@ -1,22 +1,33 @@
-import pyscipopt_gasse as pyscipopt
+import learn2branch.utilities as l2b
 
 
 class NodeBipartite:
 
-    def reset(self, model):
-        raise NotImplementedError
+    def before_reset(self, model):
+        self.buffer = {}
 
-    def obtain_observation(self, model):
-        pyscipopt_model = model.as_pyscipopt()
-        raise NotImplementedError
+    def extract(self, model, done):
+        if done:
+            return None
+        return l2b.extract_state(model.as_pyscipopt(), self.buffer)
 
 
 class Khalil2016:
 
-    def reset(self, model):
+    def before_reset(self, model):
+        self.buffer = {}
         raise NotImplementedError
 
-    def obtain_observation(self, model):
+    def extract(self, model, done):
+        if done:
+            return None
+
         pyscipopt_model = model.as_pyscipopt()
-        raise NotImplementedError
+
+        # Initialize root buffer for Khalil features extraction
+        if pyscipopt_model.getNNodes() == 1:
+            l2b.extract_khalil_variable_features(pyscipopt_model, [], self.buffer)
+
+        cands, *_ = pyscipopt_model.getPseudoBranchCands()
+        return l2b.extract_khalil_variable_features(pyscipopt_model, cands, self.buffer)
 
